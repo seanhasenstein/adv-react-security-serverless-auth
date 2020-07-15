1f2b8369694f59da8a1b690e99ae15a3433e10ec
Sean Hasenstein <seanhasenstein@gmail.com>
2020-07-15 18:04:00 -0500

    add inventory items call

---
+++ functions/get-inventory-items.js
@@ -0,0 +1,25 @@
+const InventoryItemConnection = require('../data/inventoryItemConnection');
+const { checkAuth } = require('../util');
+
+exports.handler = async function(event, context, callback) {
+	context.callbackWaitsForEmptyEventLoop = false;
+	try {
+		const user = await checkAuth(event);
+		const InventoryItem = await InventoryItemConnection.createConnection();
+		const items = await InventoryItem.find({
+			user: user.sub
+		});
+
+		return callback(null, {
+			statusCode: 200,
+			body: JSON.stringify(items)
+		});
+	} catch (err) {
+		return callback(null, {
+			statusCode: 400,
+			body: JSON.stringify({
+				error: err
+			})
+		});
+	}	
+};
