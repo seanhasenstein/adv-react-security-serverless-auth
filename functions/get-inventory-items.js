1a95890cf245734486e05b522c97d4700dedf2c9
Sean Hasenstein <seanhasenstein@gmail.com>
2020-07-15 22:08:34 -0500

    still not working?!?

--- functions/get-inventory-items.js
+++ functions/get-inventory-items.js
@@ -1,11 +1,11 @@
-const InventoryItemConnection = require('../data/InventoryItemConnection');
+const { createConnection } = require('../data/InventoryItemConnection');
 const { checkAuth } = require('../util');
 
 exports.handler = async function(event, context, callback) {
 	context.callbackWaitsForEmptyEventLoop = false;
 	try {
 		const user = await checkAuth(event);
-		const InventoryItem = await InventoryItemConnection.createConnection();
+		const InventoryItem = await createConnection();
 		const items = await InventoryItem.find({
 			user: user.sub
 		});
